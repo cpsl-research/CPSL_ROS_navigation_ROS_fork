aa3eb9bda6886fd99a3b5f0404df6ca0550592fa
davidmhunt <david.hunt@duke.edu>
2024-04-15 16:57:06 -0400

    updated map server - bounding boxes - point clouds

--- map_server/scripts/map_server_sensor_updates.py
+++ map_server/scripts/map_server_sensor_updates.py
@@ -6,9 +6,10 @@ from std_msgs.msg import Header
 from sensor_msgs.msg import PointCloud2
 import sensor_msgs.point_cloud2 as pc2
 from tf2_sensor_msgs.tf2_sensor_msgs import do_transform_cloud
+from tf2_geometry_msgs.tf2_geometry_msgs import do_transform_pose
 from nav_msgs.msg import OccupancyGrid,MapMetaData
-from geometry_msgs.msg import Pose,Point,Quaternion
-from tf.transformations import quaternion_from_euler
+from geometry_msgs.msg import Pose,Point,Quaternion,PoseStamped
+from tf.transformations import quaternion_from_euler,euler_from_quaternion
 import tf2_ros
 from jsk_recognition_msgs.msg import BoundingBox,BoundingBoxArray
 
@@ -47,6 +48,16 @@ class MapServerSensorUpdates:
             default='/lidar_cloudout'
         )
 
+        #specify ROS params - for using PointCloud2 message to update the map
+        self.bounding_box_updates_enabled = rospy.get_param(
+            param_name='~bounding_box_updates_enabled',
+            default=False
+        )
+        self.bounding_box_updates_topic = rospy.get_param(
+            param_name="~bounding_box_updates_topic",
+            default='/bbox_3d_array'
+        )
+
         #static map characteristics
         self.map_resolution:float = 0.0
         self.map_width:int = 0
@@ -70,6 +81,10 @@ class MapServerSensorUpdates:
         self.point_cloud_sub:rospy.Subscriber = None
         self.point_cloud_mask_latest = None
 
+        #bounding box listener
+        self.bounding_box_sub:rospy.Subscriber = None
+        self.bounding_box_mask_latest = None
+        self.bounding_box_mask_history = None
 
         #initialize the map
         self.init_map_server()
@@ -77,6 +92,9 @@ class MapServerSensorUpdates:
         #start the map update functions if enabled
         if self.point_cloud_updates_enabled:
             self.point_cloud_sub_init()
+        if self.bounding_box_updates_enabled:
+            self.bounding_box_history_reset()
+            self.bounding_box_sub_init()
 
         #start a time to run the node at the given intervals
         rospy.Timer(
@@ -126,11 +144,13 @@ class MapServerSensorUpdates:
             name=self.map_topic_dynamic,
             data_class=OccupancyGrid,
             queue_size=1,
-            latch=True
+            latch=True,
+            
         )
         rospy.loginfo(
-            "Dynamic map server publishing at topic: {}".format(
-                self.map_topic_dynamic
+            "Dynamic map server publishing on topic: {} at rate: {}Hz".format(
+                self.map_topic_dynamic,
+                self.update_rate_Hz
             ))
 
         return
@@ -221,13 +241,19 @@ class MapServerSensorUpdates:
     def update_map(self,event):
         
         #start with the static map
-        self.dynamic_map = self.static_map
+        self.dynamic_map = self.static_map.copy()
 
         if self.point_cloud_updates_enabled and \
             (self.point_cloud_mask_latest is not None):
             
             #denote the occupied regions
             self.dynamic_map[self.point_cloud_mask_latest] = 100
+        
+        if self.bounding_box_updates_enabled and \
+            (self.bounding_box_mask_latest is not None):
+
+            self.dynamic_map[self.bounding_box_mask_history] = 100
+            self.bounding_box_history_reset()
 
         #convert map to map message
         occupancy_grid_msg = self.get_occupancy_grid_msg(
@@ -236,7 +262,6 @@ class MapServerSensorUpdates:
 
         #publish the occupancy grid map
         self.map_pub_dynamic.publish(occupancy_grid_msg)
-        rospy.loginfo("Dynamic Map Updated")
         return
     
     def get_occupancy_grid_msg(self,map_data:np.ndarray):
@@ -346,6 +371,214 @@ class MapServerSensorUpdates:
         mask[y_indicies,x_indicies] = True
 
         return mask
+    
+    ####################################################################
+    # Updating map from the bounding boxes
+    ####################################################################
+    def bounding_box_sub_init(self):
+
+        self.bounding_box_sub = rospy.Subscriber(
+            name=self.bounding_box_updates_topic,
+            data_class=BoundingBoxArray,
+            callback=self.bounding_box_sub_callback,
+            queue_size=1
+        )
+
+        return
+    
+    def bounding_box_sub_callback(self,msg:BoundingBoxArray):
+
+        #transform the point cloud to the map frame
+        bounding_box_array_transformed = self.bounding_box_array_transform_to_map_frame(msg)
+        
+        if bounding_box_array_transformed is not None:
+
+            self.bounding_box_mask_latest = \
+                self.bounding_box_array_get_occupied_mask(
+                    bounding_box_array_transformed)
+            
+            self.bounding_box_mask_history = \
+                self.bounding_box_mask_history | \
+                self.bounding_box_mask_latest
+
+
+        return
+    
+    def bounding_box_history_reset(self):
+        self.bounding_box_mask_history = np.zeros(
+            shape=(self.map_height,self.map_width),
+            dtype=np.bool
+        )
+    
+    def bounding_box_array_transform_to_map_frame(self,
+                                            bounding_box_array:BoundingBoxArray
+                                            )->BoundingBoxArray:
+        transformed_bounding_box_array = BoundingBoxArray()
+
+        #initialize the header
+        header = Header()
+        header.frame_id = self.map_frame_id
+        header.stamp = rospy.Time.now()
+
+        transformed_bounding_box_array.header = header
+
+        for bounding_box in bounding_box_array.boxes:
+
+            #try to get the transformation
+            try:
+
+                tf_lidar_to_map = self.tf_buffer.lookup_transform(
+                    target_frame=self.map_frame_id,
+                    source_frame=bounding_box.header.frame_id,
+                    time=rospy.Time(secs=0), #get the latest transformation
+                    timeout=rospy.Duration(secs=1.0)
+                )
+            
+            except (tf2_ros.LookupException, tf2_ros.ExtrapolationException) as ex:
+                rospy.logwarn("Error looking up transformation: {}".format(ex))
+                return None
+            try:
+
+                box_pose_stamped = PoseStamped()
+                box_pose_stamped.pose = bounding_box.pose
+                box_pose_stamped.header = bounding_box.header
+
+                transformed_pose = do_transform_pose(
+                    pose=box_pose_stamped,
+                    transform=tf_lidar_to_map
+                )
+
+                transformed_box = bounding_box
+                transformed_box.header.frame_id = self.map_frame_id
+                transformed_box.pose = transformed_pose.pose
+
+                transformed_bounding_box_array.boxes.append(transformed_box)
+            
+            except tf2_ros.TransformException as ex:
+
+                rospy.logwarn("Error transforming point cloud: {}".format(ex))
+
+                return None
+        
+        return transformed_bounding_box_array
+
+    def bounding_box_array_get_occupied_mask(self,bounding_box_array:BoundingBoxArray):
+
+        final_mask = np.zeros(
+            shape=(self.map_height,self.map_width),
+            dtype=bool
+        )
+
+        for box in bounding_box_array.boxes:
+
+            box_mask = self.bounding_box_get_occupied_mask(box)
+
+            final_mask = final_mask | box_mask
+
+        return final_mask
+
+    def bounding_box_get_occupied_mask(self,bounding_box:BoundingBox):
+
+        #get the x and y verticies
+        dim_x = bounding_box.dimensions.x
+        dim_y = bounding_box.dimensions.y
+
+        verticies = 0.5 * np.array([
+            [dim_x, dim_y],
+            [dim_x,-1 * dim_y],
+            [-1* dim_x,-1*dim_y],
+            [-1* dim_x,dim_y]
+        ])
+
+        #get the rotation
+        roll,pitch,yaw = euler_from_quaternion(
+            quaternion=[
+                bounding_box.pose.orientation.x,
+                bounding_box.pose.orientation.y,
+                bounding_box.pose.orientation.z,
+                bounding_box.pose.orientation.w
+            ]
+        )
+
+        R = self.get_rot_matrix(yaw)
+
+        #get the translation
+        trans = np.array([
+            bounding_box.pose.position.x,
+            bounding_box.pose.position.y
+        ])
+
+        #get the verticies in the global frame
+        verticies = (verticies @ R.T) + trans
+        #convert to map indicies
+        map_indicies = self.map_indicies_from_points(verticies)
+
+        #generate the map
+        mask = np.zeros(
+            shape=(self.map_height,self.map_width),
+            dtype=np.uint8
+        )
+        mask_filled = cv2.fillPoly(mask,[map_indicies],1)
+
+        non_zero_idxs = np.nonzero(mask_filled)
+
+        mask = np.zeros(
+            shape=(self.map_height,self.map_width),
+            dtype=bool
+        )
+
+        mask[non_zero_idxs[0],non_zero_idxs[1]] = True
+        return mask
+    
+    def get_rot_matrix(self, rot_angle_rad):
+        """Get a rotation matrix for a given rotation angle
+
+        Args:
+            rot_angle_rad (double): the desired rotation angle in radians
+
+        Returns:
+            np.ndarray: a 2x2 rotation matrix for the desired rotation
+        """
+        return np.array([
+            [np.cos(rot_angle_rad), -1.0 * np.sin(rot_angle_rad)],
+            [np.sin(rot_angle_rad), np.cos(rot_angle_rad)]
+        ])
+
+
+    ####################################################################
+    # Other helper functions
+    ####################################################################
+    def map_indicies_from_points(self,points:np.ndarray):
+
+        x_indicies = np.floor(
+            (points[:,0] - self.map_origin.position.x) / self.map_resolution
+            ).astype(int)
+
+        y_indicies = (
+            (points[:,1] - self.map_origin.position.y) / self.map_resolution
+            ).astype(int)
+        
+        #make sure that the indicies are valid
+        valid_x_idxs = (x_indicies >= 0) & (x_indicies <= self.map_width)
+        valid_y_idxs = (y_indicies >= 0) & (y_indicies <= self.map_height)
+        valid_idxs = valid_x_idxs & valid_y_idxs
+
+        #filter the points to only valid points
+        x_indicies = x_indicies[valid_idxs]
+        y_indicies = y_indicies[valid_idxs]
+
+        points = np.array(
+            list(
+                zip(
+                    x_indicies,y_indicies
+                )
+            )
+        )
+
+        return points
+
+
+
 
     
 
